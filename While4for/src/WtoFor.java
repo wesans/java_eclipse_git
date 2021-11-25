ac471b072a5102b8c487e66e1ba90fae3e3ea6d9
ms2458 <ms2458@DESKTOP-1BL4BMG>
2021-11-25 23:31:17 +0900

    와일문 포문으로 바꾸기

---
+++ While4for/src/WtoFor.java
@@ -0,0 +1,19 @@
+
+public class WtoFor {
+
+	public static void main(String[] args) {
+	
+		for(int limit=10,x=1;x<=limit;x++) {
+			for(int n=x,i=0;i<n;i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			}
+		for(int x=9;x>=1;x--) {
+			for(int n=x,i=0;i<n;i++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+			}
+		}
+}
