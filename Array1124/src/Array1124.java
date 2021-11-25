613dd0cc357ab25fa0e26be1c04499bcd4df0f66
ms2458 <ms2458@DESKTOP-1BL4BMG>
2021-11-25 23:22:18 +0900

    array1124 했던거

---
+++ Array1124/src/Array1124.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Array1124 {
+
+	public static void main(String[] args) {
+		//자바의 배열 안에는 다 똑같은 타입만 넣기 가능..최근엔 배열 잘 안쓴다는듯?
+//		자스와 달리 자바는 길이고정, 데이터 타입 고정.
+//		int[] ar;
+//		int ar[];
+//		float arf[];
+//		String ars[];
+//		String[] ars; //바로 윗줄과 동일
+		
+//		int[] ar= {3,12,7,4,-1,-17,0}; //이거 주로 쓰고
+		
+//		int ar1[]= {3,12,7,4,-1,-17,0}; //동일 형태
+		
+//		int ar2[]=null;
+//		ar2=new int[] {3,12,7,4,-1,-17,0};
+		
+//		int[] ar3=new int[7]; 
+//		ar3[0]=3;
+//		ar3[1]=12;
+////		...
+//		ar3[7]=0;    //일케 주로 씀 빈 배열 길이 정해주고 각각 넣어주는거로
+		
+//		int[] ar4=new int[] {3,12,7,4,-1,-17,0};
+		
+//		System.out.println(ar3.length);
+//		-----
+//		String ars[]= {"Hello","World","Le Monde","NY Times"};
+//		System.out.println(ars[0]); //Hello
+//		System.out.println(ars[1]); //World
+		
+		
+	}
+}
