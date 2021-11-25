368c4b361a057f292580b5ef4c3a159d03e78f9b
ms2458 <ms2458@DESKTOP-1BL4BMG>
2021-11-25 23:29:52 +0900

    소수구하기 모르겠음ㅋ

---
+++ PrimeNumber/src/Prime.java
@@ -0,0 +1,36 @@
+
+public class Prime {
+
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		
+		//
+		// 1000까지의 피보나치 수열 구하기
+//		int a=1,b=1,c=a+b;//여기서 c=1; 해줘도 오케이인듯
+//		while(c<1000) {
+//			System.out.println(c);
+//			a=b;
+//			b=c;
+//			c=a+b;
+//		}
+                                   //또는
+//		int a=1,b=1,c=1;
+//		do {
+//			a=b;
+//			b=c;
+//			c=a+b;
+//		}while(c<1000);
+		//
+//		for(int a=1,b=1,c=1;c<1000;a=b,b=c,c=a+b) {
+//			System.out.println(c);
+//			}
+		//여기까지 피보나치 수열 만드는법 몇가지
+		
+		
+	}
+}
