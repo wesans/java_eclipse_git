3d31dcd156dab200afca69dcd11591c2ca24907f
wesans <wesans1234@gmail.com>
2021-12-06 17:00:53 +0900

    inheritan

--- DriverExample/src/DriverExample.java
+++ DriverExample/src/DriverExample.java
@@ -8,6 +8,16 @@ public class DriverExample {
 	
 	driver.driver(bus);  //자동형변환(자식 ->부모)
 	driver.driver(taxi); //자동형변환(자식 ->부모)
+	
+	if( bus instanceof Bus) {
+		System.out.println("bus instanceof Bus.");
+	} else {
+		System.out.println("bus NOT instanceof Bus.");
+		}
+	if( bus instanceof Vehicle) {
+		System.out.println("bus instanceof Vehicle.");
+	} else {
+		System.out.println("bus NOT instanceof Bus.");
+		}
 	}
-
 }
